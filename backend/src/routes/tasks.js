f293ed342fac94ab222fad1789a2ddd732a49e9e
Cleiton <cleiton__br@hotmail.com>
2026-07-12 20:38:18 -0300

    feat(backend): protege rotas de tasks com authMiddleware

--- backend/src/routes/tasks.js
+++ backend/src/routes/tasks.js
@@ -4,33 +4,16 @@ import { getTasks, getTaskById, createTask, updateTask, deleteTask } from "../co
 
 import { validateTask } from "../middlewares/taskValidation.js";
 import { validateTaskId } from "../middlewares/taskIdValidation.js";
+import { authMiddleware } from "../middlewares/authMiddleware.js";
 
 const router = Router();
 
+router.use(authMiddleware);
+
 router.get("/", getTasks);
-
-router.get(
-  "/:id",
-  validateTaskId,
-  getTaskById
-);
-
-router.post(
-  "/",
-  validateTask,
-  createTask
-);
-
-router.put(
-  "/:id",
-  validateTaskId,
-  updateTask
-);
-
-router.delete(
-  "/:id",
-  validateTaskId,
-  deleteTask
-);
+router.get("/:id", validateTaskId, getTaskById);
+router.post("/", validateTask, createTask);
+router.put("/:id", validateTaskId, updateTask);
+router.delete("/:id", validateTaskId, deleteTask);
 
 export default router;
