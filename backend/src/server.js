73ea7f2b7514727765ab9df1c1fa1e8b9fef54d4
Cleiton <cleiton__br@hotmail.com>
2026-07-11 23:57:11 -0300

    feat(backend): protege rotas com middleware

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,24 @@
 import express from 'express';
 import cors from 'cors';
-
-import errorHandler from "./middlewares/errorHandler.js";
-import tasksRoutes from "./routes/tasks.js";
+import errorHandler from './middlewares/errorHandler.js';
+import tasksRoutes from './routes/tasks.js';
+import authRoutes from './routes/auth.js';
 
 const app = express();
-app.use(cors());
+
+app.use(cors({
+  origin: 'http://localhost:5173',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+}));
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.json({ message: 'API DevTasks!' });
-    });
-    
+});
+
+app.use('/auth', authRoutes);
 app.use('/tasks', tasksRoutes);
 app.use(errorHandler);
 
