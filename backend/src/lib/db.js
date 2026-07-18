d3e49660b2d04d207c83b477f36fee4c23f1236d
Cleiton <cleiton__br@hotmail.com>
2026-07-18 00:14:37 -0300

    feat(docker): adiciona e configura conteiner com Docker Compose

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,12 +2,6 @@ import pkg from 'pg';
 
 const { Pool } = pkg;
 
-const pool = new Pool({
-  user: "devtasks_user",
-  host: "localhost",
-  database: "devtasks",
-  password: "123456",
-  port: 5432,
-});
+const pool = new Pool({ connectionString: process.env.DATABASE_URL || 'postgresql://devtasks_user:123456@localhost:5432/devtasks' });
 
 export default pool;
